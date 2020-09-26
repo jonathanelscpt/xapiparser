d60ee660e0d17d47d1b991291af0ae22af8df6ac
jonathanelscpt <jonathanelscpt@gmail.com>
2020-09-26 21:22:51 +0200

    fixed bug with cli tool if no expression was supplied

--- src/xapiparser/cli.py
+++ src/xapiparser/cli.py
@@ -7,7 +7,7 @@ from xapiparser import __version__
 from xapiparser import parse
 
 parser = argparse.ArgumentParser(description='xAPI ssh expression parser.')
-parser.add_argument('expression', metavar='expression', nargs=1,
+parser.add_argument('expression', metavar='expression', nargs='?',
                     help="xAPI ssh expression")
 parser.add_argument('--version', action="store_true",
                     help="version")
@@ -18,4 +18,7 @@ def main(args=None):
     if args.version:
         print(f"version: {__version__}")
         sys.exit()
+    if not args.expression:
+        print("Error.  No xAPI ssh expression supplied.")
+        sys.exit(1)
     print(etree.tostring(parse(args.expression[0]), pretty_print=True, encoding='unicode'))
