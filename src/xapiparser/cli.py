3dc4996d048f673a3f2adf1004ec8c3ce78dd796
jonathanelscpt <jonathanelscpt@gmail.com>
2020-09-26 21:43:25 +0200

    cleaned up cli

--- src/xapiparser/cli.py
+++ src/xapiparser/cli.py
@@ -9,16 +9,12 @@ from xapiparser import parse
 parser = argparse.ArgumentParser(description='xAPI ssh expression parser.')
 parser.add_argument('expression', metavar='expression', nargs='?',
                     help="xAPI ssh expression")
-parser.add_argument('--version', action="store_true",
-                    help="version")
+parser.add_argument('--version', action='version',
+                    version='%(prog)s v{version}'.format(version=__version__))
 
 
 def main(args=None):
     args = parser.parse_args(args=args)
-    if args.version:
-        print(f"version: {__version__}")
-        sys.exit()
     if not args.expression:
-        print("Error.  No xAPI ssh expression supplied.")
-        sys.exit(1)
-    print(etree.tostring(parse(args.expression[0]), pretty_print=True, encoding='unicode'))
+        parser.error("Error.  No xAPI ssh expression supplied.")
+    print(etree.tostring(parse(args.expression), pretty_print=True, encoding='unicode'))
