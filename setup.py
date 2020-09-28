39ee3be2b38d5214a8dce9fb7223e3119c0a6e9d
jonathanelscpt <jonathanelscpt@gmail.com>
2020-09-28 20:57:39 +0200

    Bump version: 0.0.11 → 0.0.12

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*names, **kwargs):
 
 setup(
     name='xapiparser',
-    version='0.0.11',
+    version='0.0.12',
     license='MIT',
     description='XAPI ssh command parser to convert cli commands to xml for use in the Cisco XAPI REST API',
     long_description='%s\n%s' % (
